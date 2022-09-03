4640f93b0f419fcd06f9d3ab078ef28458355e80
basantapoudel123 <basanta.poudel1221@gmail.com>
2022-09-03 18:42:52 +0545

    hcecee

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,10 +1,94 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import './Login.css'
 
-const Login = () => {
-    return(
-        <div>
-            <h1>I am Login Page</h1>
+function Login() {
+  const initialValues = {username: "", email:"", password:""};
+  const[formValues, setFormValues] = useState(initialValues);
+  const[formErrors, setFormErrors] = useState({});
+  const[isSubmit, setIsSubmit] = useState(false);
+ 
+  const handleChange = e => {
+    const  {name, value} = e.target;
+    setFormValues({ ...formValues, [name]: value});
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    setFormErrors(validate(formValues));
+    setIsSubmit(true);
+  }
+  useEffect(() => {
+    console.log(formErrors);
+    if(Object.keys(formErrors).length === 0 && isSubmit) {
+      console.log(formValues);
+    }
+  },[formErrors]);
+
+  const validate = (values) => {
+    const errors = {};
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+    if (!values.username) {
+      errors.username = "Username is required!";
+    } 
+    if(!values.email){
+      errors.email = "Email is required!";
+    }else if(!regex.test(values.email)){
+      errors.email = "This is not a valid email format!";
+    }
+    if(!values.password) {
+      errors.password = "Password is required!";
+    }else if(values.password.length < 4 ){
+      errors.password = "Password must be greater than 4 characters";
+    }else if(values.password.length > 10){
+      errors.password = "Password should not exceed 10 characters";
+    }
+    return errors;
+  }
+
+  return (
+    <div className="container">
+      {Object.keys(formErrors).length === 0 && isSubmit ? (<div className='ui message success'> Signed in Successful!! </div>
+      ) : (
+        <pre>{JSON.stringify(formValues, undefined, 2)}</pre>
+      )}
+
+      
+      <form onSubmit={handleSubmit}>
+        <h1>Login Form</h1>
+        <div className="ui divider"></div>
+        <div className="ui form">
+        <div className="field">
+          <label>Username</label>
+          <input type="text" 
+                name='username'
+                placeholder='Username' 
+                value={formValues.username}
+                onChange={handleChange}/>
         </div>
-)}
+        <p>{formErrors.username}</p>
+        <div className="field">
+          <label>Email</label>
+          <input type="email" 
+                name='email' 
+                placeholder='Email' 
+                value={formValues.email}
+                onChange={handleChange}/>
+        </div>
+        <p>{formErrors.email}</p>
+        <div className="field">
+          <label>Password</label>
+          <input type="password" 
+                name='password' 
+                placeholder='Password' 
+                value={formValues.password}
+                onChange={handleChange}/>
+        </div>
+        <p>{formErrors.password}</p>
+        <button className='fluid ui button blue'>Submit</button>
+        </div>
+      </form>
+    </div>
+  )
+}
 
-export default Login;
+export default Login
